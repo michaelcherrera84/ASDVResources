649790c174cda5db048cb030461bfa074ed2e421
Michael C. Herrera <04.ideas-amici@icloud.com>
2025-10-21 20:37:37 -0500

    Add Maven web app creation tutorial

--- src/pages/TutorialsPage.tsx
+++ src/pages/TutorialsPage.tsx
@@ -1,4 +1,4 @@
-import {Box} from "@mui/material";
+import {Box, Typography, Link, Card} from "@mui/material";
 import ImageTutorial from "../components/tutorials/ImageTutorial.tsx";
 
 function TutorialsPage() {
@@ -10,6 +10,22 @@ function TutorialsPage() {
             <ImageTutorial imageKey="common-visual-studio-error"
                            title="Common Visual Studio Error"
             />
+
+            <Box sx={{p: {xs: 2, sm: 4}, mx: "auto", w: "100%"}}>
+                <Typography variant="h4" gutterBottom mt={2}>
+                    Other Tutorials
+                </Typography>
+                <Card style={{display: 'flex', flexWrap: 'wrap', gap: 10, padding: 15}}>
+                    <Link
+                        href="/tutorials/netbeans-maven-web-application-tutorial.html"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        underline="none"
+                    >
+                        NetBeans Maven Web Application Tutorial (JSF/PrimeFaces/GlassFish)
+                    </Link>
+                </Card>
+            </Box>
         </Box>
     );
 }
